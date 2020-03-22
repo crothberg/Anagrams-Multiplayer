d0292edfac494caa47f6d89dd55b3721ad614b1d
Ben Chaney <bichaney@wpi.edu>
2020-03-22 17:24:24 -0400

    Fix db schema

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ db = psycopg2.connect(DATABASE_URL, sslmode='require')
 def setup_db():
     cur = db.cursor()
     cur.execute('CREATE TABLE USERS (                   \
-                    SID INT PRIMARY KEY     NOT NULL,   \
+                    SID TEXT PRIMARY KEY    NOT NULL,   \
                     NAME    TEXT            NOT NULL,   \
                     GAME    TEXT                    )')
     cur.execute('CREATE TABLE GAMES (                   \
@@ -60,7 +60,7 @@ def join_game(command):
         ##create the game:
         cur.execute('INSERT INTO GAMES (NAME) VALUES (%s)', (game_name,))
 
-    cur.execute('INSERT INTO USERS (SID, NAME, GAME) VALUES (%s, %s, %s)', (str(sid), username, game_name))
+    cur.execute('INSERT INTO USERS (SID, NAME, GAME) VALUES (%s, %s, %s)', (sid, username, game_name))
 
     new_state = generate_game_state(cur, game_name)
     cur.execute('SELECT SID FROM USERS WHERE GAME = %s', (game_name,))
