a11d77f11c9ec54def9ace01885ad1d889f332c6
Carmi Rothberg <crothber@brandeis.edu>
2020-03-22 00:34:14 -0400

    some updates to html form input (still not ready)

--- static/js/anagrams.js
+++ static/js/anagrams.js
@@ -1,16 +1,18 @@
-var socket = io();
+window.onload = function() {
+    $("#flip-action").focus();
+    var socket = io();
 
-socket.on('connect', function() {
-    socket.emit('json', {command: 'join_game', game_name: 'my_game'});
-});
+    socket.on('connect', function() {
+        socket.emit('json', {command: 'join_game', game_name: 'my_game'});
+    });
 
-socket.on('json', function() {
-    alert('received callback!');
-});
+    socket.on('json', function() {
+        alert('received callback!');
+    });
 
-// window.onload = function() {
-//     alert($("body").html());
-//     $("body").addEventListener("keyup", function(event) {
-//         alert('pressed enter!');
-//     });
-// }
+    $("#flip-action").on("keyup", function(event) {
+        if (event.key == 'Enter') {
+            console.log($(this).text)
+        }
+    });
+}
