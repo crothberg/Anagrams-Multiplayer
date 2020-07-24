2ae2d99e23de6de328b1e01759d41ef57a964f0f
Ben Chaney <Chaneybenjamini@gmail.com>
2020-07-23 20:02:59 -0400

    Fix syntax errors

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO
 import flask_socketio
-import game_data
+from game_data import game_room
 import psycopg2
 import os
 import json
