ee639ad0cb2fab092a97454c4ba8cf26f2e828ac
Carmi Rothberg <crothber@brandeis.edu>
2020-08-01 21:54:11 -0400

    front-end / style changes

--- static/js/anagrams.js
+++ static/js/anagrams.js
@@ -13,43 +13,36 @@ window.onload = function() {
         socket.emit('join_game', {'username': username, 'game_name': game_name});
     });
 
+    /* TO DO */
+    // socket.emit('get_game_state', {'game_name': game_name})
+
     socket.on('user_added', function(data) {
         $("#player-space").append(make_player(data['username']));
     });
 
     socket.on('game_state_update', function(data) {
+
         console.log('GAME STATE UPDATED')
         console.log(data);
         var status = data['status'];
         var users = data['game_state']['users'];
         var middle = data['game_state']['middle'];
         
-        // // Update status
-        // var determiner = 'a'
-        // if ('erioasfhlx'.includes(letter)) {determiner='an'};
-        // $('#status').text(user+' flipped '+determiner+' \''+letter+'\'');
+        // Update status
         $('#status').text(status);
         
-        // Update all users simultaneously
-        // $("#player-space").html(make_player_spaces(data['users']));
         // Update middle
         $('#middle').html(make_middle(middle));
-    });
-
-    socket.on('word_stolen', function(data) {
-        console.log(data);
-        user = data['user'];
-        word = data['word'];
-        source = data['source'];
-
-        // Update status
-        $('#status').text(user+' took the word \''+word+'\' from '+source+'!')
+        
+        // Update all users simultaneously
+        $("#player-space").html(make_all_players(users));
     });
 
     $("#flip-action").focus();
     $("#flip-action").submit(function(event) {
+
         // Get user input on press enter
-        word = $("#flip-action-text").val();
+        word = $("#flip-action-text").val().toUpperCase();
         if (!word) {
             // If just enter (with no word), flip tile
             socket.emit('flip', {'user': username, 'room': game_name});
@@ -83,8 +76,30 @@ window.onload = function() {
         )
     });
 
-    $("#undo").click(function() {
-        socket.emit('undo');
+    socket.on('challenge', function(data) {
+        console.log('Challege initiated.')
+        $("#challenge-status").text(data['status']);
+        $("#modal-background").show('fade', 'slow');
+        $("#challenge-modal").show('bounce', {times: 1}, 'slow');
+    });
+
+    $("#challenge").click(function() {
+        socket.emit('challenge', {'room': game_name, 'user': username});
+        console.log('Challenge request sent.')
+    });
+
+    $("#vote-yes").click(function() {
+        socket.emit('vote', {'room': game_name, 'user': username, 'vote': 'accept'});
+        $("#modal-background").hide('fade', 'slow');
+        $("#challenge-modal").hide('drop', {direction: 'up'}, 'slow');
+        console.log('Voted accept.')
+    });
+
+    $("#vote-no").click(function() {
+        socket.emit('vote', {'room': game_name, 'user': username, 'vote': 'reject'});
+        $("#modal-background").hide('fade', 'slow');
+        $("#challenge-modal").hide('drop', {direction: 'up'}, 'slow');
+        console.log('Voted reject.')
     });
 
 }
