63be92e3991d17f17228d581ae1ef855acc68132
Ben Chaney <Chaneybenjamini@gmail.com>
2020-08-02 20:16:56 -0400

    Force rebuild

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ import psycopg2
 DATABASE_URL = os.environ['DATABASE_URL']
 db = psycopg2.connect(DATABASE_URL, sslmode='allow')
 db.autocommit = True
-
+ 
 def print_log_line(log_line):
     cur = db.cursor()
     cur.execute('INSERT INTO LOGS (LOG_LINE, TIME) VALUES (%s, NOW())', (log_line,))
